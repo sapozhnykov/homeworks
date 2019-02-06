2f0e7244b765956eac1e773afafcc22d011088bf
Sapozhnykov Andrii <sapozhnikov.andrew9@gmail.com>
2019-02-06 22:58:05 +0200

    use camelCase

--- src/com/luxoft/java/course/sapozhnykov/homework/algorithm/sorting/insertion/InsertionSort.java
+++ src/com/luxoft/java/course/sapozhnykov/homework/algorithm/sorting/insertion/InsertionSort.java
@@ -6,9 +6,9 @@ public class InsertionSort {
     public static int[] sort(int[] ints) {
         int[] newArray = Arrays.copyOf(ints, ints.length);
 
-        for(int cur_index = 1; cur_index < newArray.length; cur_index++) {
-            int cur_element = newArray[cur_index];
-            int j = cur_index;
+        for(int curIndex = 1; curIndex < newArray.length; curIndex++) {
+            int cur_element = newArray[curIndex];
+            int j = curIndex;
             while (j > 0 && newArray[j - 1] > cur_element) {
                 newArray[j] = newArray [j - 1];
                 j = j - 1;
