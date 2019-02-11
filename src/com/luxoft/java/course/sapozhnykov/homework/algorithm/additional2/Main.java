467cc8e75748d24fbeab2f94002e113212656bf1
Sapozhnykov Andrii <sapozhnikov.andrew9@gmail.com>
2019-02-11 13:24:52 +0200

    add new additional exercise

---
+++ src/com/luxoft/java/course/sapozhnykov/homework/algorithm/additional2/Main.java
@@ -0,0 +1,7 @@
+package com.luxoft.java.course.sapozhnykov.homework.algorithm.additional2;
+
+public class Main {
+    public static void main(String[] args) {
+        FuncCalculator.start();
+    }
+}
